facc20896daa81e66c9c74a71c80e67018eab59f
miuel <rivasmig@gmail.com>
2025-04-29 23:13:12 +0200

    styled AllEthnicity

--- src/app/all-ethicity/page.tsx
+++ src/app/all-ethicity/page.tsx
@@ -54,28 +54,8 @@ const AllEthnicity: React.FC = () => {
   }, []);
 
   return (
-    <div className="mx-auto max-w-[1512px] flex justify-center items-center h-screen flex-col mb-10 relative">
-      <a
-        href="/dashboard"
-        className="hidden md:flex absolute text-sm top-52 left-0 w-auto p-4 rounded-sm bg-asafeBlack text-white  h-12"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-4 w-4 mr-2 -rotate-90"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          strokeWidth={2}
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M3 12h18m-9 9l9-9-9-9"
-          />
-        </svg>
-        Back Dashboard
-      </a>
-      <div className="flex gap-2 overflow-hidden">
+    <div className="ml-5 md:ml-0 md:mx-auto max-w-[1512px] flex justify-center h-auto flex-col mb-10 relative">
+      <div className="md:flex gap-2 overflow-hidden hidden">
         {imageArr.map((item, index) => (
           <Reveal key={index} delay={index * 0.2}>
             <img
@@ -97,20 +77,49 @@ const AllEthnicity: React.FC = () => {
         ))}
       </div>
 
-      <h1 className="italic font-bold text-7xl font-editorialNew mt-5">
-        All Human
-      </h1>
-      <h1 className="ml-14 text-7xl">Ethnicities</h1>
-
-      <p className="py-6 max-w-xl">
-        Where every culture, color, and story matters. Dive into a curated space
-        that embraces global diversity, creativity, and unity without borders.
-        Explore a dynamic database showcasing detailed records, histories, and
-        identities from communities around the world — all organized in an
-        intuitive and powerful way.
-      </p>
+      <Reveal>
+        <h1 className="italic font-bold text-5xl xl:text-[270px] font-editorialNew mt-5 leading-none">
+          All Human
+        </h1>
+      </Reveal>
+      <div className="relative">
+        <Reveal>
+          <h1 className="md:ml-14 text-5xl  md:text-7xl">Ethnicities</h1>
+        </Reveal>
+        <Reveal>
+          <p className="py-6 max-w-xl">
+            Where every culture, color, and story matters. Dive into a curated
+            space that embraces global diversity, creativity, and unity without
+            borders. Explore a dynamic database showcasing detailed records,
+            histories, and identities from communities around the world — all
+            organized in an intuitive and powerful way.
+          </p>
+        </Reveal>
+        <Reveal>
+          <a
+            href="/dashboard"
+            className="hidden md:flex absolute text-sm bottom-5 right-0 w-auto p-4 rounded-sm bg-asafeBlack text-white  h-12"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-4 w-4 mr-2 -rotate-90"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3 12h18m-9 9l9-9-9-9"
+              />
+            </svg>
+            Back Dashboard
+          </a>
+        </Reveal>
+      </div>
       {loading ? (
-        <div className="flex justify-center items-center h-screen">
+        <div className="flex justify-center items-center max-h-96">
           <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-gray-900"></div>
         </div>
       ) : (
